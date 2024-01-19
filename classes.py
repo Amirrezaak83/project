2a56d0f55ca561d737bac555e12fb6baf1df2baf
saraghayoori <156826841+saraghayoori@users.noreply.github.com>
2024-01-19 20:24:20 +0330

    Add files via upload
    تقسیم تابع main بین توابع user_login و user_menu
    لحاظ کردن این مورد که کاربر باید همیشه قابلیت خروج از برنامه را داشته باشد.

--- classes.py
+++ classes.py
@@ -1,6 +1,9 @@
 import sqlite3
 import requests
 from datetime import datetime
+import random
+import string
+
 class User:
     def __init__(self, username, name, email, password, user_type, logged_in):
         self.username = username
@@ -439,40 +442,90 @@ def main():
     while True:
         print("1. register account")
         print("2. login account")
-        select_options = input("Select option:  ")
+        print("3. exit program")
+        select_options = input("Select option: ")
+
         if select_options == '1':
             User.register_account()
-        if select_options == '2':
-            print("1. login with your username and password")
-            print("2. login with one time password")
-            login_method = input("select an option From above: ")
-            if login_method == '1':
-                username = input("Enter your username: ")
-                password = input("Enter your password: ")
-                User.login(username, password)
-                with sqlite3.connect("Clinic Database.sql") as Clinic_database:
-                    cursor =  Clinic_database.cursor()
-                    cursor.execute('''SELECT * FROM Users WHERE username = ? AND password = ?''', (username, password))
-                    existing_user = cursor.fetchone()
-                    if existing_user is not None:
-                        if existing_user[5] == 'p':
-                            print("1. make appoinment")
-                            print("2. cancell appoinment")
-                            print("3. view appoinment")
-                            print("4. logout")
-                            print("5. updaate inoformation")
-                            patient_options  = input("select your option: ")
-                            if patient_options == '1':
-                                Appoinment.make_appoinment(username, password)
-                            elif select_options == '2':
-                                Appoinment.cancell_appoinment(username, password)  
-                            elif select_options == '3':
-                                User.view_appoinment(username, password)
-                            elif select_options == '4':
-                                User.logout(username, password)
-                            elif select_options == '5' :
-                                if existing_user[5] == 'p':
-                                    User.Update_profile(username, password)
-            
- 
+        elif select_options == '2':
+            user_logged_in = False
+            username = input("Enter your username: ")
+            if user_login():
+                user_logged_in = True
+                user_menu(username)
+            else:
+                print("Login failed or cancelled. Returning to main menu.")
+
+        elif select_options == '3':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid option selected.")
+
+
+
+def user_login():
+    print("1. login with your username and password")
+    print("2. login with one time password")
+    login_method = input("Select an option from above: ")
+
+
+    if login_method == '1':
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        if User.login(username, password):
+            with sqlite3.connect("Clinic Database.sql") as Clinic_database:
+                cursor = Clinic_database.cursor()
+                cursor.execute("SELECT * FROM Users WHERE username = ? AND password = ?", (username, password))
+                existing_user = cursor.fetchone()
+                if existing_user:
+                    return True
+        return False
+    elif login_method == '2':
+        username = input("Enter your username: ")
+        characters = string.ascii_uppercase + string.ascii_letters + string.digits + string.ascii_lowercase
+        generated_password = "".join(random.choices(characters, k=8))
+        print("Your one-time password is:", generated_password)
+        otp = input("Enter the one-time password: ")
+        if otp == generated_password:
+            User.login_with_generated_password(username)
+            with sqlite3.connect('''Clinic Database.sql''') as Clinic_database:
+                cursor = Clinic_database.cursor()
+                cursor.execute('''SELECT * From Users WHERE username = ?''',(username,))
+                existing_user = cursor.fetchone()
+                return True
+        return False
+    
+    else:
+        print("Invalid login method.")
+        return False
+
+
+def user_menu(username):
+    while True:
+        print("1. make appointment")
+        print("2. cancel appointment")
+        print("3. view appointment")
+        print("4. update information")
+        print("5. logout and return to main menu")
+        patient_options = input("Select your option: ")
+
+        if patient_options == '1':
+            Appoinment.make_appoinment(username)
+        elif patient_options == '2':
+            Appoinment.cancel_appoinment(username)
+        elif patient_options == '3':
+            User.view_appoinment(username)
+        elif patient_options == '4':
+            User.update_profile(username)
+        elif patient_options == '5':
+            print("Logging out...")
+            break
+        else:
+            print("Invalid option selected.")
+
+if __name__ == "__main__":
+    main()
+
+
 
